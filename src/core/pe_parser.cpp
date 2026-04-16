e928f02f861b7de9ed830fbf386884972965a4ef
Arknight38 <arkknight38@gmail.com>
2026-04-15 20:57:28 -0600

    Replace LIEF with manual PE parser and remove LIEF dependency
    - Replaced LIEF-based PE parser with manual PE structure parsing
    - Removed LIEF include path from project
    - Removed LIEF config.h stub (no longer needed)
    - Project now builds without external library dependencies

--- src/core/pe_parser.cpp
+++ src/core/pe_parser.cpp
@@ -1,16 +1,5 @@
 #include "core/pe_parser.h"
-
-#ifdef _MSC_VER
-#pragma warning(push, 0)
-#endif
-#include <LIEF/PE.hpp>
-#ifdef _MSC_VER
-#pragma warning(pop)
-#endif
-
-#include <cstdint>
-#include <memory>
-#include <utility>
+#include <cstring>
 
 namespace atlus {
 
@@ -20,59 +9,195 @@ const PESection* PEInfo::find_section(const std::string& name) const {
     return nullptr;
 }
 
+// Minimal PE structures for manual parsing
+#pragma pack(push, 1)
+struct DOSHeader {
+    uint16_t e_magic;
+    uint16_t e_cblp;
+    uint16_t e_cp;
+    uint16_t e_crlc;
+    uint16_t e_cparhdr;
+    uint16_t e_minalloc;
+    uint16_t e_maxalloc;
+    uint16_t e_ss;
+    uint16_t e_sp;
+    uint16_t e_csum;
+    uint16_t e_ip;
+    uint16_t e_cs;
+    uint16_t e_lfarlc;
+    uint16_t e_ovno;
+    uint16_t e_res[4];
+    uint16_t e_oemid;
+    uint16_t e_oeminfo;
+    uint16_t e_res2[10];
+    uint32_t e_lfanew;
+};
+
+struct FileHeader {
+    uint16_t machine;
+    uint16_t num_sections;
+    uint32_t timestamp;
+    uint32_t symbol_table;
+    uint32_t num_symbols;
+    uint16_t optional_header_size;
+    uint16_t characteristics;
+};
+
+struct DataDirectory {
+    uint32_t rva;
+    uint32_t size;
+};
+
+struct OptionalHeader32 {
+    uint16_t magic;
+    uint8_t major_linker;
+    uint8_t minor_linker;
+    uint32_t code_size;
+    uint32_t data_size;
+    uint32_t bss_size;
+    uint32_t entry_point;
+    uint32_t code_base;
+    uint32_t data_base;
+    uint32_t image_base;
+    uint32_t section_align;
+    uint32_t file_align;
+    uint16_t major_os;
+    uint16_t minor_os;
+    uint16_t major_image;
+    uint16_t minor_image;
+    uint16_t major_subsystem;
+    uint16_t minor_subsystem;
+    uint32_t win32_version;
+    uint32_t image_size;
+    uint32_t headers_size;
+    uint32_t checksum;
+    uint16_t subsystem;
+    uint16_t dll_characteristics;
+    uint32_t stack_reserve;
+    uint32_t stack_commit;
+    uint32_t heap_reserve;
+    uint32_t heap_commit;
+    uint32_t loader_flags;
+    uint32_t num_data_dirs;
+    DataDirectory data_dirs[16];
+};
+
+struct OptionalHeader64 {
+    uint16_t magic;
+    uint8_t major_linker;
+    uint8_t minor_linker;
+    uint32_t code_size;
+    uint32_t data_size;
+    uint32_t bss_size;
+    uint32_t entry_point;
+    uint32_t code_base;
+    uint64_t image_base;
+    uint32_t section_align;
+    uint32_t file_align;
+    uint16_t major_os;
+    uint16_t minor_os;
+    uint16_t major_image;
+    uint16_t minor_image;
+    uint16_t major_subsystem;
+    uint16_t minor_subsystem;
+    uint32_t win32_version;
+    uint32_t image_size;
+    uint32_t headers_size;
+    uint32_t checksum;
+    uint16_t subsystem;
+    uint16_t dll_characteristics;
+    uint64_t stack_reserve;
+    uint64_t stack_commit;
+    uint64_t heap_reserve;
+    uint64_t heap_commit;
+    uint32_t loader_flags;
+    uint32_t num_data_dirs;
+    DataDirectory data_dirs[16];
+};
+
+struct SectionHeader {
+    char name[8];
+    uint32_t virtual_size;
+    uint32_t virtual_address;
+    uint32_t raw_size;
+    uint32_t raw_address;
+    uint32_t reloc_address;
+    uint32_t line_numbers;
+    uint16_t num_relocs;
+    uint16_t num_lines;
+    uint32_t characteristics;
+};
+#pragma pack(pop)
+
+static constexpr uint16_t PE_MAGIC = 0x5A4D; // "MZ"
+static constexpr uint32_t PE_SIGNATURE = 0x00004550; // "PE\0\0"
+static constexpr uint16_t PE32_MAGIC = 0x10b;
+static constexpr uint16_t PE32_PLUS_MAGIC = 0x20b;
+
 atlus::PEInfo atlus::PEParser::parse(const BinaryFile& file) {
     PEInfo info;
-    if (file.empty())
+    if (file.size() < sizeof(DOSHeader))
         return info;
 
-    std::unique_ptr<LIEF::PE::Binary> binary = LIEF::PE::Parser::parse(file.data);
-    if (!binary)
+    const auto* dos = reinterpret_cast<const DOSHeader*>(file.bytes());
+    if (dos->e_magic != PE_MAGIC)
         return info;
 
-    const auto& hdr = binary->header();
-    const auto& opt = binary->optional_header();
+    if (dos->e_lfanew + 4 > file.size())
+        return info;
 
-    info.machine   = static_cast<uint16_t>(hdr.machine());
-    info.is_64bit  = (opt.magic() == LIEF::PE::PE_TYPE::PE32_PLUS);
-    info.entry_rva = opt.addressof_entrypoint();
-    info.image_base = opt.imagebase();
-    info.subsystem = static_cast<uint16_t>(opt.subsystem());
+    const auto* pe_sig = reinterpret_cast<const uint32_t*>(file.bytes() + dos->e_lfanew);
+    if (*pe_sig != PE_SIGNATURE)
+        return info;
 
-    for (const LIEF::PE::Section& section : binary->sections()) {
-        PESection sec;
-        sec.name        = section.name();
-        sec.vaddr       = static_cast<uint32_t>(section.virtual_address());
-        sec.vsize       = section.virtual_size();
-        sec.raw_offset  = section.pointerto_raw_data();
-        sec.raw_size    = section.sizeof_raw_data();
-        sec.flags       = section.characteristics();
+    size_t offset = dos->e_lfanew + 4;
+    const auto* file_hdr = reinterpret_cast<const FileHeader*>(file.bytes() + offset);
+    offset += sizeof(FileHeader);
 
-        const auto content = section.content();
-        sec.data.assign(content.begin(), content.end());
+    info.machine = file_hdr->machine;
 
-        if (sec.data.empty() && sec.raw_size > 0) {
-            const size_t end = size_t(sec.raw_offset) + size_t(sec.raw_size);
-            if (end <= file.size()) {
-                const auto* base = file.bytes() + sec.raw_offset;
-                sec.data.assign(base, base + sec.raw_size);
-            }
+    if (file_hdr->optional_header_size > 0) {
+        const auto* opt_magic = reinterpret_cast<const uint16_t*>(file.bytes() + offset);
+        if (*opt_magic == PE32_MAGIC) {
+            info.is_64bit = false;
+            const auto* opt = reinterpret_cast<const OptionalHeader32*>(file.bytes() + offset);
+            info.entry_rva = opt->entry_point;
+            info.image_base = opt->image_base;
+            info.subsystem = opt->subsystem;
+            offset += sizeof(OptionalHeader32);
+        } else if (*opt_magic == PE32_PLUS_MAGIC) {
+            info.is_64bit = true;
+            const auto* opt = reinterpret_cast<const OptionalHeader64*>(file.bytes() + offset);
+            info.entry_rva = opt->entry_point;
+            info.image_base = opt->image_base;
+            info.subsystem = opt->subsystem;
+            offset += sizeof(OptionalHeader64);
+        } else {
+            offset += file_hdr->optional_header_size;
         }
-
-        info.sections.push_back(std::move(sec));
     }
 
-    if (binary->has_imports()) {
-        for (const LIEF::PE::Import& imp : binary->imports()) {
-            ImportEntry ie;
-            ie.dll = imp.name();
-            for (const LIEF::PE::ImportEntry& e : imp.entries()) {
-                if (e.is_ordinal())
-                    ie.functions.push_back("ord#" + std::to_string(e.ordinal()));
-                else if (!e.name().empty())
-                    ie.functions.push_back(e.name());
-            }
-            info.imports.push_back(std::move(ie));
+    for (uint16_t i = 0; i < file_hdr->num_sections; ++i) {
+        const auto* sec = reinterpret_cast<const SectionHeader*>(file.bytes() + offset);
+        offset += sizeof(SectionHeader);
+
+        PESection pesec;
+        pesec.name = std::string(sec->name, 8);
+        // Trim nulls from name
+        if (auto pos = pesec.name.find('\0'); pos != std::string::npos)
+            pesec.name.resize(pos);
+        pesec.vaddr = sec->virtual_address;
+        pesec.vsize = sec->virtual_size;
+        pesec.raw_offset = sec->raw_address;
+        pesec.raw_size = sec->raw_size;
+        pesec.flags = sec->characteristics;
+
+        if (sec->raw_address + sec->raw_size <= file.size()) {
+            pesec.data.assign(file.bytes() + sec->raw_address,
+                            file.bytes() + sec->raw_address + sec->raw_size);
         }
+
+        info.sections.push_back(std::move(pesec));
     }
 
     info.valid = true;
